59cfa5f9bf732e5bd7feed79d79d8bf8343799e0
thaunghtike-share <87596555+thaunghtike-share@users.noreply.github.com>
2022-04-16 15:52:01 +0630

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-locals {
-    aws_region  = "us-east-1"
-    prefix      = "tho-terraform-remote-state"
-    ssm_prefix  = "/terraform/lab"
-    common_tags = {
-        Project         = "ECS_Terraform"
-        ManagedBy       = "Terraform"
-    }
-}
